cab2eefee8fb46e21488fd17be0d6d4bf6cd93f9
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-20 17:28:31 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
-# Add your code here
+
+
+class Dog 
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    self.save
+  end
+    
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all
+    self.all.each do |person|
+      puts "#{person.name}"
+    end
+  end 
+
+  def save
+  
+    @@all << self
+  end
+  
+end
