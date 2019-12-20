8dfd374f5bbd6510117c4a3421a6846536e42858
Sehrish Balouch <sehrishbalouch4@gmail.com>
2019-12-20 01:00:35 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
-# Add your code here
+class Dog 
+  
+  @@all = [ ]
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+def self.print_all
+  print all 
+end 
+def save
+  @all << self 
+end 
+def .clear_all
+  all.clear 
+end 
+end 
