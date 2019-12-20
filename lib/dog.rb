0483373325770aeabc07a20b82df345514bfdaf1
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-20 01:00:09 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Add your code here
+class Dog
+  attr_accessor :name
+  @@all = []
+  def initialize (name)
+    @name = name
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |id|
+      puts id.name
+    end
+  end
+  
+  def save
+    @@all << self
+  end 
+  
+  def self.clear_all
+    #Array.Clear(@@all, 0, @@all.length)
+    @@all = []
+  end
+end
