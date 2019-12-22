1c4545802cdca16c59af94d9da4d9f7c0487a2af
Ho Yin Keung <hykeung92@gmail.com>
2019-12-22 21:30:50 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,12 +9,18 @@ class Dog
   end 
   
   def self.all 
-    @@all.each do |dog|
-		puts dog.name
+    @@all
 	end
   
-#   def self.print_all
-#     self.each {|name| puts name}
-#     name
-#   end
+  def self.print_all
+    puts @@all.map{ |dog| dog.name }
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
 end
